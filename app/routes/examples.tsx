49ccb62ac8741ebf99f31c69a7dd2b9ae9129a59
Vanja Kondac <vanja.kondac@axiomq.com>
2025-05-14 14:55:22 +0200

    change text

--- app/routes/examples.tsx
+++ app/routes/examples.tsx
@@ -39,7 +39,7 @@ export default function ExamplesLayout() {
           to="/examples/doctor"
           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
         >
-          Doctor Map
+          Doctors Map
         </Link>
       </div>
       <Outlet />
